fc3649bfe8febfabed3c2d47691f760ae9861219
Ekaterina <kat-erina-07@mail.ru>
2023-06-22 00:55:13 +0400

    Sum of digits in a  number

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 27: Напишите программу, которая принимает на вход число и выдаёт 
+сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+int InputNum(string message)
+{
+    Console.WriteLine(message);
+    return int.Parse(Console.ReadLine()!);
+}
+int SumDigitsNumber(int number)
+{
+    int res = 0;
+    while (number != 0)
+    {
+        res += number % 10;
+        number = number / 10;
+    }
+    return res;
+}
+int num = InputNum("Введите целое число: ");
+int sum = SumDigitsNumber(num);
+Console.Write($"Cумма цифр в числе равна {sum}");
